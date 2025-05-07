651440591aa8a6389741aac1715336cb3115ad07
Ainsworth_Adam <ainsa128@hsd.k12.or.us>
2025-05-07 10:57:43 -0700

    added basic obstacle spawning and player movement

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float speed = 30;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
